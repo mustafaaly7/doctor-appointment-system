b91b519846fbd149c41d4370fb6178a30135acd7
Mustafa Ali <mustafaaaly27@gmail.com>
2024-10-30 21:06:40 +0500

    created header and hero section

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,16 @@
 import Header from "@/components/header";
+import Hero from "@/components/hero";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   // on installation of next js when asked to add  turbo pack do yes cuz it makes it faster rn we making this project on next 14 instead of 15 cuz alot of libraries aint working simultaneouly fine
 
   return (
-    <div>
+    <div className="container mx-auto">
 <Header/>
+<Hero/>
+
+
 
 
     </div>
