f99d577e61f35f72ab0624c827258011384644be
Mustafa Ali <mustafaaaly27@gmail.com>
2024-11-09 16:22:41 +0500

    added async function user callback data thru google into mongodb

--- auth.js
+++ auth.js
@@ -1,5 +1,22 @@
+import Connectdb from "@/lib/connectDb"
+import { UserModel } from "@/lib/models/userModel"
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
+
+
+const RegisterUser = async (obj) => {
+  await Connectdb()
+  const user = await UserModel.findOne({ email: obj.email })
+  if (user) {
+    return user
+  } else {
+    let newUser = await UserModel(obj);
+    newUser = await newUser.save();
+    return newUser
+  }
+}
+
+
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google],
@@ -7,12 +24,20 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     async signIn({ account, profile }) {
 
       if (account.provider === "google") {
-        console.log("account=> ",account);
-        console.log("profile => ",profile);
-
+        console.log("account=> ", account);
+        console.log("profile => ", profile);
         
+        
+        const obj = {
+        firstName: profile.given_name,
+          lastName: profile.family_name,
+          email: profile.email,
+          picture: profile.picture,
+        }
+        const user = await RegisterUser(obj)
+        return user // Do different verification for other providers that don't have `email_verified`
       }
-      return true // Do different verification for other providers that don't have `email_verified`
-    },}
+    },
+  }
 
 })
