e7cb0393bf55f7ff61bd4d8fee1c3747ad90c3fb
Mustafa Ali <mustafaaaly27@gmail.com>
2024-11-09 15:34:53 +0500

    added async calback function on auth.js

--- auth.js
+++ auth.js
@@ -3,4 +3,16 @@ import Google from "next-auth/providers/google"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google],
+  callbacks: {
+    async signIn({ account, profile }) {
+
+      if (account.provider === "google") {
+        console.log("account=> ",account);
+        console.log("profile => ",profile);
+
+        
+      }
+      return true // Do different verification for other providers that don't have `email_verified`
+    },}
+
 })
